101c292a2560342469b4c3072d66a2ff2429fc07
mishapadidar <mishapadidar14@gmail.com>
2025-04-29 18:18:44 -0400

    added plot to coil forces

--- forces/compute_coil_forces.py
+++ forces/compute_coil_forces.py
@@ -2,6 +2,7 @@ import numpy as np
 from simsopt._core import load
 from simsopt.field.selffield import regularization_circ
 from simsopt.field.force import coil_force, LpCurveForce
+from simsopt.geo import curves_to_vtk
 
 
 """
@@ -19,6 +20,19 @@ bsurfs = data[0] # BoozerSurfaces
 biotsavart = bsurfs[current_group_idx].biotsavart
 
 coils = biotsavart.coils
+curves = [coil.curve for coil in coils]
 for ii, c in enumerate(coils):
     force = np.linalg.norm(coil_force(c, coils, regularization_circ(0.05)), axis=1)
     print(f"max force on coil {ii}", np.max(np.abs(force)))
+
+def pointData_forces(coils):
+    forces = []
+    for c in coils:
+        force = np.linalg.norm(coil_force(c, coils, regularization_circ(0.05)), axis=1)
+        force = np.append(force, force[0])
+        forces = np.concatenate([forces, force])
+    point_data = {"F": forces}
+    return point_data
+
+curves_to_vtk(curves, "./curves_with_forces", close=True, extra_data=pointData_forces(coils))
+
