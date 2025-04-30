025dddee1f4b3ec508f23f481067ecc9ad2440b4
mishapadidar <mishapadidar14@gmail.com>
2025-04-30 11:08:20 -0400

    got radius of coils

--- forces/compute_coil_forces.py
+++ forces/compute_coil_forces.py
@@ -11,44 +11,47 @@ Compute the coil forces following the example,
 https://github.com/hiddenSymmetries/simsopt/blob/master/examples/3_Advanced/coil_forces.py
 """
 
-design = "A"
+minor_radius = 0.054 # 54mm
 
+designs = ["A", "B"]
+for des in designs:
+    # load the boozer surfaces (1 per Current configuration, so 3 total.)
+    data = load(f"../designs/design{des}_after_scaled.json")
+    bsurfs = data[0] # BoozerSurfaces
+    magnetic_axis_curves = data[2] # magnetic axis CurveRZFouriers
 
-# load the boozer surfaces (1 per Current configuration, so 3 total.)
-data = load(f"../designs/design{design}_after_scaled.json")
-bsurfs = data[0] # BoozerSurfaces
-magnetic_axis_curves = data[2] # magnetic axis CurveRZFouriers
-
-print("Design", design)
-# 3 different iota optimizations
-for iota_group_idx in range(3):
     print("")
-    print("iota group:", iota_group_idx)
-    biotsavart = bsurfs[iota_group_idx].biotsavart
+    print("-"*50)
+    print("Design", des)
+    # 3 different iota optimizations
+    for iota_group_idx in range(3):
+        print("")
+        print("iota group:", iota_group_idx)
+        biotsavart = bsurfs[iota_group_idx].biotsavart
 
-    # check mean |B| on axis
-    axis_curve = magnetic_axis_curves[iota_group_idx]
-    xyz = axis_curve.gamma()
-    biotsavart.set_points(xyz)
-    modB = biotsavart.AbsB()
-    print("mean |B| on axis: %.4f"%(np.mean(modB)))
+        # check mean |B| on axis
+        axis_curve = magnetic_axis_curves[iota_group_idx]
+        xyz = axis_curve.gamma()
+        biotsavart.set_points(xyz)
+        modB = biotsavart.AbsB()
+        print("mean |B| on axis: %.4f"%(np.mean(modB)))
 
-    coils = biotsavart.coils
-    curves = [coil.curve for coil in coils]
-    for ii, c in enumerate(coils):
-        force = np.linalg.norm(coil_force(c, coils, regularization_circ(0.05)), axis=1)
-        print(f"max force on coil {ii}: %.2f"%(np.max(np.abs(force))))
+        coils = biotsavart.coils
+        curves = [coil.curve for coil in coils]
+        for ii, c in enumerate(coils):
+            force = np.linalg.norm(coil_force(c, coils, regularization_circ(minor_radius)), axis=1)
+            print(f"max force on coil {ii}: %.2f"%(np.max(np.abs(force))))
 
-    def pointData_forces(coils):
-        forces = []
-        for c in coils:
-            force = np.linalg.norm(coil_force(c, coils, regularization_circ(0.05)), axis=1)
-            force = np.append(force, force[0])
-            forces = np.concatenate([forces, force])
-        point_data = {"F": forces}
-        return point_data
+        def pointData_forces(coils):
+            forces = []
+            for c in coils:
+                force = np.linalg.norm(coil_force(c, coils, regularization_circ(minor_radius)), axis=1)
+                force = np.append(force, force[0])
+                forces = np.concatenate([forces, force])
+            point_data = {"F": forces}
+            return point_data
 
-    outdir = "./output"
-    os.makedirs(outdir, exist_ok=True)
-    curves_to_vtk(curves, outdir + f"/design_{design}_group_{iota_group_idx}_curves_with_forces", close=True, extra_data=pointData_forces(coils))
+        outdir = "./output"
+        os.makedirs(outdir, exist_ok=True)
+        curves_to_vtk(curves, outdir + f"/design_{des}_group_{iota_group_idx}_curves_with_forces", close=True, extra_data=pointData_forces(coils))
 
