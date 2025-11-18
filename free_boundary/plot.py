58dab85c1c357303f1f0bd96ea9799c2ba92f140
mishapadidar <mishapadidar14@gmail.com>
2025-11-18 12:04:02 -0800

    updated free boundayr plot

--- free_boundary/plot.py
+++ free_boundary/plot.py
@@ -7,7 +7,7 @@ import pickle
 import pandas as pd
 plt.rc('font', family='serif')
 # plt.rc('text.latex', preamble=r'\\usepackage{amsmath,bm}')
-plt.rcParams.update({'font.size': 12})
+plt.rcParams.update({'font.size': 16})
 colors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", 
           "tab:brown", "tab:pink", "tab:gray", "tab:olive", "tab:cyan"]
 # colors = ['lightcoral', 'goldenrod', 'mediumseagreen','orange', "lightskyblue", "plum"]
@@ -55,27 +55,27 @@ df = pd.DataFrame(df_data)
 df.sort_values(by=['beta', 'Imin'], inplace=True)
 print(df)
 
-fig, (ax1,ax2,ax3) = plt.subplots(figsize=(12,4), ncols=3)
+fig, (ax1,ax2,ax3) = plt.subplots(figsize=(13,4), ncols=3)
 
 for ii, beta in enumerate(df['beta'].unique()):
     if beta == 0.0:
         continue
     df_sub = df[df['beta']==beta]
-    ax1.plot(df_sub['I0'], df_sub['qs_err'],   marker=markers[ii], color=colors[ii], markersize=4, label=f'beta={beta}%')
-    ax2.plot(df_sub['I0'], df_sub['iota'], marker=markers[ii], color=colors[ii], markersize=4, label=f'beta={beta}%')
-    ax3.plot(df_sub['I0'], df_sub['xp_shift'],   marker=markers[ii], color=colors[ii], markersize=4, label=f'beta={beta}%')
+    ax1.plot(df_sub['I0'], df_sub['qs_err'],   lw=3, marker=markers[ii], color=colors[ii-1], markersize=6, label=f'beta={beta}%')
+    ax2.plot(df_sub['I0'], df_sub['iota'], lw=3, marker=markers[ii], color=colors[ii-1], markersize=6, label=f'beta={beta}%')
+    ax3.plot(df_sub['I0'], df_sub['xp_shift'],  lw=3, marker=markers[ii], color=colors[ii-1], markersize=6, label=f'beta={beta}%')
 
-# plot the expected configuration as a star
-df_sub = df[(df['beta']==0.01) & (df['Imin']==0.01)]
-ax1.scatter(df_sub['I0'], df_sub['qs_err'],   marker='*', s=100, color='red', label=f'TJ-K scale',zorder=100)
-ax2.scatter(df_sub['I0'], df_sub['iota'], marker='*', s=100, color='red', label=f'TJ-K scale',zorder=100)
-ax3.scatter(df_sub['I0'], df_sub['xp_shift'],   marker='*', s=100, color='red', label=f'TJ-K scale',zorder=100)
+# # plot the expected configuration as a star
+# df_sub = df[(df['beta']==0.01) & (df['Imin']==0.01)]
+# ax1.scatter(df_sub['I0'], df_sub['qs_err'],   marker='*', s=100, color='red', label=f'TJ-K scale',zorder=100)
+# ax2.scatter(df_sub['I0'], df_sub['iota'], marker='*', s=100, color='red', label=f'TJ-K scale',zorder=100)
+# ax3.scatter(df_sub['I0'], df_sub['xp_shift'],   marker='*', s=100, color='red', label=f'TJ-K scale',zorder=100)
 
 
 ax1.set_xlabel(r'$I_0$ [A]')
 ax1.set_ylabel('$J_{QS}$ [%]')
 ax1.grid(color='lightgrey', linestyle='--', linewidth=0.5)
-ax1.axhline(qs_err_vac, color='black', linestyle=':', label='vacuum')
+ax1.axhline(qs_err_vac, color='black', linestyle=':', lw=2, label='vacuum')
 
 gap_scale = 3
 qs_gap = np.abs(df[(df['beta']==0.1) & (df['Imin']==0.0)].qs_err.item() - qs_err_vac)
@@ -84,16 +84,16 @@ ax1.set_ylim(qs_err_vac-gap_scale*qs_gap, qs_err_vac+gap_scale*qs_gap)
 
 ax2.set_xlabel(r'$I_0$ [A]')
 ax2.set_ylabel(r'$\iota_{edge}$')
-ax2.axhline(iota_vac, color='black', linestyle=':', label='vacuum')
+ax2.axhline(iota_vac, color='black', linestyle=':', lw=2,label='vacuum')
 ax2.grid(color='lightgrey', linestyle='--', linewidth=0.5)
 # ax2.set_ylim(0.18, 0.20)
 iota_gap = np.abs(df[(df['beta']==0.1) & (df['Imin']==0.0)].iota.item() - iota_vac)
 ax2.set_ylim(iota_vac-gap_scale*iota_gap, iota_vac+gap_scale*iota_gap)
-ax2.legend(loc='upper left', fontsize=10)
+ax2.legend(loc='upper left', fontsize=14)
 
 ax3.set_xlabel(r'$I_0$ [A]')
 ax3.set_ylabel('X-point displacement [mm]')
-ax3.axhline(0.0, color='black', linestyle=':', label='vacuum')
+ax3.axhline(0.0, color='black', linestyle=':', lw=2,label='vacuum')
 ax3.grid(color='lightgrey', linestyle='--', linewidth=0.5)
 # ax3.set_ylim(-0.3, 5.0)
 xp_shift_gap = np.abs(df[(df['beta']==0.1) & (df['Imin']==0.0)].xp_shift.item())
